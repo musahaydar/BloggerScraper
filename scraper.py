3c856d362f661a49db32c9956489ca1d437d6a92
Musa Haydar <musah@umich.edu>
2024-08-20 10:17:49 -0400

    Keep browser open for better performance

--- scraper.py
+++ scraper.py
@@ -5,13 +5,20 @@ from requests_html import HTMLSession
 from pyppeteer import launch
 from pathlib import Path
 
-def scrapePage(url):
+async def scrapePage(url):
     session = HTMLSession()
+    browser = await launch()
+    page = await browser.newPage()
+
     res = session.get(url)
     while res:
         filename = re.match(".+(\/[^.]+).html$", url)
-        asyncio.get_event_loop().run_until_complete(generatePDF(url, "output/" + filename.group(1) + ".pdf"))
         print(url)
+
+        # get page as html
+        await page.goto(url)
+        await page.pdf({'path': "output/" + filename.group(1) + ".pdf", 'format': 'A4'})
+
         # get the next post
         next = res.html.find('.blog-pager-older-link', first=True)
         if next:
@@ -19,20 +26,14 @@ def scrapePage(url):
             res = session.get(url)
         else:
             res = None
+    
+    await browser.close()    
 
-async def generatePDF(url, pdf_path):
-    browser = await launch()
-    page = await browser.newPage()
-    await page.goto(url)
-    await page.pdf({'path': pdf_path, 'format': 'A4'})
-    await browser.close()
 
 def main():
     url = input("Enter the URL of the latest post: ")
-
     # create output dir
     Path("output").mkdir(exist_ok=True)
-
-    scrapePage(url)
+    asyncio.get_event_loop().run_until_complete(scrapePage(url))
 
 main()
